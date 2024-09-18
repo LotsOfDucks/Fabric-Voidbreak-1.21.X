59b55cc5e79318b773940833ad646a2159340508
LotsOfDucks <113879225+LotsOfDucks@users.noreply.github.com>
2024-09-18 15:14:34 -0700

    Lumei Melon Cleanup
    Signed-off-by: LotsOfDucks <113879225+LotsOfDucks@users.noreply.github.com>

--- src/main/java/com/lotsofducks/voidbreak/block/custom/CeilingStemBlock.java
+++ src/main/java/com/lotsofducks/voidbreak/block/custom/CeilingStemBlock.java
@@ -2,84 +2,109 @@ package com.lotsofducks.voidbreak.block.custom;
 
 import com.lotsofducks.voidbreak.block.ModBlocks;
 import com.lotsofducks.voidbreak.item.ModItems;
+import com.mojang.serialization.MapCodec;
 import net.minecraft.block.*;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.mob.RavagerEntity;
 import net.minecraft.item.ItemConvertible;
+import net.minecraft.item.ItemStack;
 import net.minecraft.registry.tag.BlockTags;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.IntProperty;
 import net.minecraft.state.property.Properties;
-import net.minecraft.state.property.Property;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
-import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.random.Random;
 import net.minecraft.util.shape.VoxelShape;
-import net.minecraft.world.BlockView;
-import net.minecraft.world.World;
-import net.minecraft.world.WorldAccess;
-import net.minecraft.world.WorldView;
+import net.minecraft.world.*;
 
-public class CeilingStemBlock extends CropBlock {
+public class CeilingStemBlock extends PlantBlock {
 
     public static final int MAX_AGE = 4;
-    public static final IntProperty AGE = Properties.AGE_4;
-    private static final VoxelShape[] AGE_TO_SHAPE = new VoxelShape[]{
-            Block.createCuboidShape(7.0, 15.0, 7.0, 9.0, 16.0, 9.0),
-            Block.createCuboidShape(7.0, 12.0, 7.0, 9.0, 16.0, 9.0),
-            Block.createCuboidShape(7.0, 8.0, 7.0, 9.0, 16.0, 9.0),
-            Block.createCuboidShape(6.0, 4.0, 6.0, 10.0, 16.0, 10.0),
-            Block.createCuboidShape(6.0, 0.0, 6.0, 10.0, 16.0, 10.0)
-    };
+    public static final IntProperty AGE;
+    private static final VoxelShape[] AGE_TO_SHAPE;
+    static {
+        AGE = Properties.AGE_4;
+        AGE_TO_SHAPE = new VoxelShape[]{
+                Block.createCuboidShape(7.0, 15.0, 7.0, 9.0, 16.0, 9.0),
+                Block.createCuboidShape(7.0, 12.0, 7.0, 9.0, 16.0, 9.0),
+                Block.createCuboidShape(7.0, 8.0, 7.0, 9.0, 16.0, 9.0),
+                Block.createCuboidShape(6.0, 4.0, 6.0, 10.0, 16.0, 10.0),
+                Block.createCuboidShape(6.0, 0.0, 6.0, 10.0, 16.0, 10.0)
+        };
+    }
+    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+        builder.add(AGE);
+    }
+
+    protected IntProperty getAgeProperty() {
+        return AGE;
+    }
+
+    public int getMaxAge() {
+        return 4;
+    }
 
     public int getAge(BlockState state) {
         return state.get(this.getAgeProperty());
     }
 
-    @Override
-    protected VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
-        return AGE_TO_SHAPE[this.getAge(state)];
+    public BlockState withAge(int age) {
+        return this.getDefaultState().with(this.getAgeProperty(), age);
     }
 
-    @Override
+    public final boolean isMature(BlockState state) {
+        return this.getAge(state) >= this.getMaxAge();
+    }
+
+    protected boolean hasRandomTicks(BlockState state) {
+        return !this.isMature(state);
+    }
+
     protected void randomTick(BlockState state, ServerWorld world, BlockPos pos, Random random) {
-        if (world.getBaseLightLevel(pos, 0) >= 10) {
+        if (world.getBaseLightLevel(pos, 0) >= 3) {
             int i = this.getAge(state);
-            if (i < MAX_AGE) {
+            if (i < (MAX_AGE-1)) {
                 if (random.nextInt(7) == 0) {
                     world.setBlockState(pos, this.withAge(i + 1), Block.NOTIFY_LISTENERS);
                 }
             }
-            if (i == MAX_AGE) {
+            if (i == (MAX_AGE-1)) {
                 BlockPos blockPos = pos.down();
-                if (world.getBlockState(blockPos).isAir()) {
+                if (random.nextInt(7) == 0 && world.getBlockState(blockPos).isAir()) {
+                    world.setBlockState(pos, this.withAge(i + 1), Block.NOTIFY_LISTENERS);
                     world.setBlockState(blockPos, ModBlocks.LUMEI_FRUIT.getDefaultState());
                 }
             }
         }
     }
 
-    @Override
-    protected int getGrowthAmount(World world) {
-        return MathHelper.nextInt(world.random, 2, 3);
-    }
-
-    @Override
     protected boolean canPlaceAt(BlockState state, WorldView world, BlockPos pos) {
-        BlockPos blockPos = pos.up();
-        return hasEnoughLightAt(world, pos) && this.canPlantBelow(world.getBlockState(blockPos));
+        BlockPos above = pos.up();
+        return hasEnoughLightAt(world, pos) && this.canPlantBelow(world.getBlockState(above));
     }
 
     protected static boolean hasEnoughLightAt(WorldView world, BlockPos pos) {
-        return world.getBaseLightLevel(pos, 0) >= 9;
+        return world.getBaseLightLevel(pos, 0) >= 0;
     }
 
-    @Override
+    protected void onEntityCollision(BlockState state, World world, BlockPos pos, Entity entity) {
+        if (entity instanceof RavagerEntity && world.getGameRules().getBoolean(GameRules.DO_MOB_GRIEFING)) {
+            world.breakBlock(pos, true, entity);
+        }
+
+        super.onEntityCollision(state, world, pos, entity);
+    }
+
     protected ItemConvertible getSeedsItem() {
         return ModItems.LUMEI_BUD;
     }
 
-    @Override
+    public ItemStack getPickStack(WorldView world, BlockPos pos, BlockState state) {
+        return new ItemStack(this.getSeedsItem());
+    }
+
     protected BlockState getStateForNeighborUpdate(
             BlockState state, Direction direction, BlockState neighborState, WorldAccess world, BlockPos pos, BlockPos neighborPos) {
         return !state.canPlaceAt(world, pos) ? Blocks.AIR.getDefaultState() : super.getStateForNeighborUpdate(state, direction, neighborState, world, pos, neighborPos);
@@ -89,7 +114,17 @@ public class CeilingStemBlock extends CropBlock {
         return floor.isIn(BlockTags.LEAVES);
     }
 
+    protected VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+        return AGE_TO_SHAPE[this.getAge(state)];
+    }
+
     public CeilingStemBlock(Settings settings) {
         super(settings);
+        this.setDefaultState(this.stateManager.getDefaultState().with(this.getAgeProperty(), 0));
+    }
+
+    @Override
+    protected MapCodec<? extends PlantBlock> getCodec() {
+        return null;
     }
 }
--- src/main/java/com/lotsofducks/voidbreak/block/custom/HangingMelonBlock.java
+++ src/main/java/com/lotsofducks/voidbreak/block/custom/HangingMelonBlock.java
@@ -1,58 +1,68 @@
 package com.lotsofducks.voidbreak.block.custom;
 
+
 import com.lotsofducks.voidbreak.block.ModBlocks;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
 import net.minecraft.block.ShapeContext;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.FallingBlockEntity;
-import net.minecraft.item.ItemConvertible;
-import net.minecraft.registry.tag.BlockTags;
+import net.minecraft.entity.mob.RavagerEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.IntProperty;
 import net.minecraft.state.property.Properties;
-import net.minecraft.state.property.Property;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
-import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.random.Random;
 import net.minecraft.util.shape.VoxelShape;
-import net.minecraft.world.BlockView;
-import net.minecraft.world.World;
-import net.minecraft.world.WorldAccess;
-import net.minecraft.world.WorldView;
+import net.minecraft.world.*;
+
+import static net.minecraft.block.FallingBlock.canFallThrough;
 
 public class HangingMelonBlock extends CeilingStemBlock{
-    public HangingMelonBlock(Settings settings) {
-        super(settings);
-    }
 
     public static final int MAX_AGE = 3;
-    public static final IntProperty AGE = Properties.AGE_3;
-    private static final VoxelShape[] AGE_TO_SHAPE = new VoxelShape[]{
-            Block.createCuboidShape(7.0, 14.0, 7.0, 9.0, 16.0, 9.0),
-            Block.createCuboidShape(5.0, 10.0, 5.0, 11.0, 16.0, 11.0),
-            Block.createCuboidShape(3.0, 6.0, 3.0, 13.0, 16.0, 13.0),
-            Block.createCuboidShape(0.0, 0.0, 0.0, 16.0, 16.0, 16.0),
-            Block.createCuboidShape(7.0, 14.0, 7.0, 9.0, 16.0, 9.0),
-            Block.createCuboidShape(5.0, 10.0, 5.0, 11.0, 16.0, 11.0),
-            Block.createCuboidShape(3.0, 6.0, 3.0, 13.0, 16.0, 13.0),
-            Block.createCuboidShape(0.0, 0.0, 0.0, 16.0, 16.0, 16.0),
-            Block.createCuboidShape(7.0, 14.0, 7.0, 9.0, 16.0, 9.0)
-    };
-
-    @Override
-    protected VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
-        return AGE_TO_SHAPE[this.getAge(state)];
+    public static final IntProperty AGE;
+    private static final VoxelShape[] AGE_TO_SHAPE;
+    static {
+        AGE = Properties.AGE_3;
+        AGE_TO_SHAPE = new VoxelShape[]{
+                Block.createCuboidShape(7.0, 14.0, 7.0, 9.0, 16.0, 9.0),
+                Block.createCuboidShape(5.0, 10.0, 5.0, 11.0, 16.0, 11.0),
+                Block.createCuboidShape(3.0, 6.0, 3.0, 13.0, 16.0, 13.0),
+                Block.createCuboidShape(0.0, 0.0, 0.0, 16.0, 16.0, 16.0)
+        };
+    }
+    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+        builder.add(AGE);
     }
 
-    @Override
+    protected IntProperty getAgeProperty() {
+        return AGE;
+    }
+
+    public int getMaxAge() {
+        return 3;
+    }
+
+    public int getAge(BlockState state) {
+        return state.get(this.getAgeProperty());
+    }
+
+    public BlockState withAge(int age) {
+        return this.getDefaultState().with(this.getAgeProperty(), age);
+    }
+
+    protected boolean hasRandomTicks(BlockState state) {
+        return !this.isMature(state);
+    }
+
     protected void randomTick(BlockState state, ServerWorld world, BlockPos pos, Random random) {
-        if (world.getBaseLightLevel(pos, 0) >= 10) {
+        if (world.getBaseLightLevel(pos, 0) >= 0) {
             int i = this.getAge(state);
             if (i < MAX_AGE) {
-                if (random.nextInt(4) == 0) {
+                if (random.nextInt(7) == 0) {
                     world.setBlockState(pos, this.withAge(i + 1), Block.NOTIFY_LISTENERS);
                 }
             }
@@ -63,36 +73,34 @@ public class HangingMelonBlock extends CeilingStemBlock{
         }
     }
 
-    @Override
-    protected ItemConvertible getSeedsItem() {
-        return ModBlocks.LUMEI_MELON.asItem();
-    }
-
-    @Override
-    protected int getGrowthAmount(World world) {
-        return MathHelper.nextInt(world.random, 1, 2);
-    }
-
-    @Override
     protected boolean canPlaceAt(BlockState state, WorldView world, BlockPos pos) {
-        BlockPos blockPos = pos.up();
-        return hasEnoughLightAt(world, pos) && this.canPlantBelow(world.getBlockState(blockPos));
+        BlockPos above = pos.up();
+        return hasEnoughLightAt(world, pos) && this.canPlantBelow(world.getBlockState(above));
     }
-
 
     protected static boolean hasEnoughLightAt(WorldView world, BlockPos pos) {
-        return world.getBaseLightLevel(pos, 0) >= 9;
+        return world.getBaseLightLevel(pos, 0) >= 0;
     }
 
-    @Override
-    protected BlockState getStateForNeighborUpdate(
-            BlockState state, Direction direction, BlockState neighborState, WorldAccess world, BlockPos pos, BlockPos neighborPos) {
-            world.scheduleBlockTick(pos, this, this.getFallDelay());
+    protected void onEntityCollision(BlockState state, World world, BlockPos pos, Entity entity) {
+        if (entity instanceof RavagerEntity && world.getGameRules().getBoolean(GameRules.DO_MOB_GRIEFING)) {
+            world.breakBlock(pos, true, entity);
+        }
+
+        super.onEntityCollision(state, world, pos, entity);
+    }
+
+    protected BlockState getStateForNeighborUpdate(BlockState state, Direction direction, BlockState neighborState, WorldAccess world, BlockPos pos, BlockPos neighborPos) {
+        world.scheduleBlockTick(pos, this, this.getFallDelay());
         return super.getStateForNeighborUpdate(state, direction, neighborState, world, pos, neighborPos);
     }
 
     protected boolean canPlantBelow(BlockState floor) {
         return floor.isOf(ModBlocks.LUMEI_STEM);
+    }
+
+    protected VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+        return AGE_TO_SHAPE[this.getAge(state)];
     }
 
     protected void onBlockAdded(BlockState state, World world, BlockPos pos, BlockState oldState, boolean notify) {
@@ -112,10 +120,11 @@ public class HangingMelonBlock extends CeilingStemBlock{
     }
 
     protected int getFallDelay() {
-        return 5;
+        return 2;
     }
 
-    public static boolean canFallThrough(BlockState state) {
-        return state.isAir() || state.isIn(BlockTags.FIRE) || state.isReplaceable();
+    public HangingMelonBlock(Settings settings) {
+        super(settings);
+        this.setDefaultState(this.stateManager.getDefaultState().with(this.getAgeProperty(), 0));
     }
 }
